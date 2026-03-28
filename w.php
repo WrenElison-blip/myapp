86a882f7a70fec239d8a5788e7bc8e0456e60303
Wren_Elison <2301726@ub.edu.ph>
2026-03-28 10:43:07 +0800

    second commit

---
+++ w.php
@@ -0,0 +1,13 @@
+<?php
+// This is a single-line comment
+echo 'Hello, World!';
+echo '<br>';
+// PHP inside HTML
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h1><?php echo 'Welcome to PHP!'; ?></h1>
+<p><?= 'Short echo tag is <?= ?>' ?></p>
+</body>
+</html>
